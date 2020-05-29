4f021f1dea3712d2dbfd5cdb46de8d2371d87d32
weizy2018 <2233467661@qq.com>
2020-05-29 18:00:05 +0800

    :pencil:

--- main_yolov3.py
+++ main_yolov3.py
@@ -7,9 +7,11 @@ import os
 rng.seed(12345)
 
 # videoPath = "my_video.mp4"
-videoPath = "/home/weizy/Downloads/VID3.mp4"
+videoPath = "VID.mp4"
 configPath = "YOLOv3/yolov3.cfg"
 weightPath = "YOLOv3/yolov3.weights"
+# configPath = "YOLOv3/yolov3-tiny.cfg"
+# weightPath = "YOLOv3/yolov3-tiny.weights"
 classesPath = "YOLOv3/coco.names"
 
 if not os.path.exists(videoPath):
@@ -24,10 +26,10 @@ if not os.path.exists(classesPath):
 with open(classesPath, 'rt') as f:
     classes = f.read().rstrip('\n').split('\n')
 
-confThreshold = 0.5  # 置信度阈值
-nmsThreshold = 0.4   #Non-maximum suppression threshold
-inpWidth = 416
-inpHeight = 416
+confThreshold = 0.5
+nmsThreshold = 0.4
+inpWidth = 320
+inpHeight = 320
 
 net = cv.dnn.readNetFromDarknet(configPath, weightPath)
 net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
@@ -38,6 +40,7 @@ def getOutputsNames(net):
     return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 # 取最大置信度的Bounding Box返回
+# 该函数不用了
 def process(outs):
     classIds = []
     confidences = []
@@ -59,6 +62,44 @@ def process(outs):
         return label
     return None
 
+def postProcess(roi, outs):
+    height, width, channel = roi.shape
+    classIds = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                center_x = (int)(detection[0] * width)
+                center_y = (int)(detection[1] * height)
+                box_width = (int)(detection[2] * width)
+                box_height = (int)(detection[3] * height)
+                left = (int)(center_x - box_width / 2)
+                top = (int)(center_y - box_height / 2)
+                classIds.append(classId)
+                confidences.append((float)(confidence))
+                boxes.append([left, top, box_width, box_height])
+    box_result = []
+    conf_result = []
+    class_result = []
+    index = cv.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
+    for i in index:
+        i = i[0]
+        box_result.append(boxes[i])
+        conf_result.append(confidences[i])
+        class_result.append(classes[classIds[i]])
+    
+    print(class_result)
+    return box_result, conf_result, class_result
+
+def transform(box, roi_x, roi_y):
+    for b in box:
+        b[0] += roi_x
+        b[1] += roi_y
+
 
 surf = cv.xfeatures2d.SURF_create(1000)
 # surf = cv.xfeatures2d.SURF_create()
@@ -68,12 +109,12 @@ index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 search_params = dict(checks = 32)
 flann = cv.FlannBasedMatcher(index_params, search_params)
 
-cv.namedWindow("match", cv.WINDOW_NORMAL)
+# cv.namedWindow("match", cv.WINDOW_NORMAL)
 # cv.namedWindow("frame1", cv.WINDOW_NORMAL)
 # cv.namedWindow("frame2", cv.WINDOW_NORMAL)
 # cv.namedWindow("after dilate", cv.WINDOW_NORMAL)
 # cv.namedWindow("draw contours", cv.WINDOW_NORMAL)
-cv.namedWindow("rectangle", cv.WINDOW_NORMAL)
+cv.namedWindow("dst", cv.WINDOW_NORMAL)
 
 capture = cv.VideoCapture(videoPath)
 if not capture.isOpened():
@@ -92,7 +133,8 @@ RECT_MAX_HIGHT = 0.5 * rows
 RECT_MIN_WIDTH = 20
 RECT_MIN_HIGHT = 20
 
-test = True
+# test = True
+test = False
 
 kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
 while True:
@@ -123,10 +165,10 @@ while True:
         
         warp = cv.warpPerspective(frame1, M, (cols, rows))
         sub = cv.absdiff(frame2, warp)
-        cv.imshow("sub", sub)
-        if (int)(capture.get(cv.CAP_PROP_POS_FRAMES)) == 166:
-            cv.imwrite("pic/sub.jpg", sub)
-            print("OK")
+        # cv.imshow("sub", sub)
+        # if (int)(capture.get(cv.CAP_PROP_POS_FRAMES)) == 166:
+        #     cv.imwrite("pic/sub.jpg", sub)
+        #     print("OK")
 
         sub = sub[20:rows - 20, 20:cols - 20]
 
@@ -143,7 +185,7 @@ while True:
 
         dst_gray = cv.morphologyEx(dst_gray, cv.MORPH_DILATE, kernel)
         dst_gray = cv.morphologyEx(dst_gray, cv.MORPH_DILATE, kernel)
-        cv.imshow("after dilate", dst_gray)
+        # cv.imshow("after dilate", dst_gray)
 
         # edges = cv.Canny(dst, 300, 450)
         # cv.imshow("edges", edges)
@@ -153,13 +195,14 @@ while True:
         if hierarchy is None:
             continue
        
-        temp = frame2.copy()
+        temp = frame2[20:rows - 20, 20:cols - 20].copy()
         for c in contours:
             rect = cv.boundingRect(c)
             color = (rng.randint(0, 255), rng.randint(0, 255), rng.randint(0, 255))
             x, y, w, h = rect
-            if cv.contourArea(c) > 200 and w < RECT_MAX_WIDTH and h < RECT_MAX_HIGHT \
-                and w > RECT_MIN_WIDTH and h > RECT_MIN_HIGHT and w * h > 5000:
+            # if cv.contourArea(c) > 200 and w < RECT_MAX_WIDTH and h < RECT_MAX_HIGHT \
+            #     and w > RECT_MIN_WIDTH and h > RECT_MIN_HIGHT and w * h > 5000:
+            if cv.contourArea(c) > 200 and w > RECT_MIN_WIDTH and h > RECT_MIN_HIGHT and w * h > 5000:
                 label = None
                 if not test:
                     roi = temp[y:y+h, x:x+w]
@@ -167,36 +210,45 @@ while True:
                     net.setInput(blob)
                     outputName = getOutputsNames(net)
                     outs = net.forward(outputName)
-                    label = process(outs)
+                    # label = process(outs)
+                    box, conf, cla = postProcess(roi, outs)
+                    transform(box, x, y)
+                    t, _ = net.getPerfProfile()
+                    # print("Inference time: %.2f ms" % (t * 1000.0 / cv.getTickFrequency()))
 
-                cv.rectangle(temp, (x, y), (x + w, y + h), color, 2)
-                if label and not test:
-                    text_size, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-                    cv.rectangle(temp, (x, y - text_size[1]), (x + text_size[0], y), color, cv.FILLED)
-                    cv.putText(temp, label, (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+                if len(box) > 0:
+                    for i, b in enumerate(box):
+                        label = f'{cla[i]} {conf[i]:.2f}'
+                        tex_size, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+                        cv.rectangle(temp, (b[0], b[1]), (b[0] + b[2], b[1] + b[3]), color, 2)
+                        cv.rectangle(temp, (b[0], b[1] - tex_size[1]), (b[0] + tex_size[0], b[1]), color, cv.FILLED)
+                        cv.putText(temp, label, (b[0], b[1]), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+                else:
+                    cv.rectangle(temp, (x, y), (x + w, y + h), color, 2)
+                # if label and not test:
+                #     text_size, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+                #     cv.rectangle(temp, (x, y - text_size[1]), (x + text_size[0], y), color, cv.FILLED)
+                #     cv.putText(temp, label, (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
         totalFrame = (int)(capture.get(cv.CAP_PROP_FRAME_COUNT))
         frameNum = (int)(capture.get(cv.CAP_PROP_POS_FRAMES))
-        if (int)(capture.get(cv.CAP_PROP_POS_FRAMES)) == 166:
-            cv.imwrite("pic/rect.jpg", temp)
-            print("OK")
-        # 166th
+        # if (int)(capture.get(cv.CAP_PROP_POS_FRAMES)) == 166:
+        #     cv.imwrite("pic/rect.jpg", temp)
+        #     print("OK")
         text = "current frame: " + str(frameNum) + "/" + str(totalFrame)
         cv.putText(temp, text, (5, 20), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
-        cv.imshow("rectangle", temp)
+        cv.imshow("dst", temp)
 
     else:
         print("not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
         matches_mask = None
     
-    draw_params = dict(matchColor = (0, 255, 0),
-                    singlePointColor = None,
-                    matchesMask = matches_mask,
-                    flags = 2)
-    match = cv.drawMatches(frame1, kp1, frame2, kp2, good, None, **draw_params)
+    # draw_params = dict(matchColor = (0, 255, 0),
+    #                 singlePointColor = None,
+    #                 matchesMask = matches_mask,
+    #                 flags = 2)
+    # match = cv.drawMatches(frame1, kp1, frame2, kp2, good, None, **draw_params)
 
-    cv.imshow("match", match)
-    # cv.imshow("frame1", frame1)
-    # cv.imshow("frame2", frame2)
+    # cv.imshow("match", match)
 
     key = cv.waitKey(20)
     if key == ord('q'):
@@ -207,7 +259,6 @@ while True:
         cv.imwrite("pic/match.jpg", match)
     if key == ord('t'):
         cv.imwrite("pic/threshold.jpg", dst_gray)
-        print(capture.get(cv.CAP_PROP_POS_FRAMES))
     
 capture.release()
 cv.destroyAllWindows()
--- test3.py
+++ test3.py
@@ -2,6 +2,26 @@ import cv2 as cv
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-img = cv.imread("pic/threshold.jpg", cv.IMREAD_COLOR)
-plt.imshow(img[:, :, ::-1])
-plt.show()
+cap = cv.VideoCapture("VID.mp4")
+if not cap.isOpened():
+    print("can't open the video")
+    exit(0)
+
+cv.namedWindow("frame", cv.WINDOW_NORMAL)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    cv.imshow("frame", frame)
+    if (int)(cap.get(cv.CAP_PROP_POS_FRAMES)) == 166:
+        cv.imwrite("pic/frame.jpg", frame)
+        print("OK")
+    
+    key = cv.waitKey(30)
+    if key == ord('q'):
+        break
+    
+
+cap.release()
+cv.destroyAllWindows()
